b6e2fafcd0f00d938cd7a6afe793495f27537eb5
meleykehuseynova <161501438+meleykehuseynova@users.noreply.github.com>
2024-03-25 21:49:40 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+         //istid=fadəcinin adi nece simvoldan ibaretdir?
+    // let name = prompt("adiniz daxil edin: ");
+   // console.log(name.length);
+
+          // Istifadeccinin daxil 1 ci etdiyi sozde 2ci simvol varmi?
+        
+    // let name = prompt("adinizi daxil edin:");
+    // let simvol= prompt("simvolu daxoil edin:");
+    // console.log(name.indexOf(simvol) !== -1);
+     
+        // Istifadecinin daxil etdiyi soz daxil etdyi simvolla baslayirmi?
+        // let name = prompt("adiniz daxil edin:");
+        // let simvol= prompt("simvolu daxil edin:");
+        // let birinci= name.charAt(0);
+        // console.log(birinci==simvol);
+
+        //Istifadecinin daxil etdiyi soz daxil etdyi simvolla bitirmi?
+    //    let name = prompt("adiniz daxil edin:");
+    //    let simvol= prompt("simvolu daxil edin:");
+    //    let sonuncu= name.charAt(name.length - 1);
+    //    console.log(sonuncu==simvol);
+
+            //Istifadecinin daxil etdiyi 1ci sozde dxail etdiyi2ci simvolun index`ini tapnn
+        // let name = prompt("sozu daxil edin:");
+        // let simvol= prompt("simvolu daxil edin:");
+        // let index = name.indexOf(simvol)
+        //  console.log("index: " + index);
+
+        // Istifadecinin daxil etdiyi sozun boyuk yada kicik oldugunu tapn
+        // let name = prompt("sozu daxil edin:");
+        // console.log( (name === name.toUpperCase()));
